a1588914e280e96fc61827e3f66523c16de0bbe2
Vinh Nguyen <xuanvinhct@gmail.com>
2015-07-05 18:41:24 +0700

    update function

--- week02/functions.hs
+++ week02/functions.hs
@@ -23,3 +23,13 @@ collatz 1 = 0
 collatz n
   | even n = 1 + collatz (n `div` 2)
   | otherwise = 1 + collatz (n*3 + 1)
+
+lfoldl :: (b -> a -> b) -> b -> [a] -> b
+lfoldl _ acc [] = acc
+lfoldl f acc (x:xs) = lfoldl f (f acc x) xs
+
+sum' :: (Num a) => [a] -> a
+sum' xs = foldl (\acc x -> acc + x) 0 xs
+
+map' :: (a -> b) -> [a] -> [b]
+map' f xs = foldr (\x acc -> f x : acc) [] xs
