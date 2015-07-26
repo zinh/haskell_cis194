3105f15b385386cb94653b1025b3cd9cf1b69d79
Vinh Nguyen <xuanvinhct@gmail.com>
2015-07-26 15:56:43 +0700

    update week3 exercise

---
+++ week03/HW03.hs
@@ -0,0 +1,159 @@
+module HW03 where
+
+data Expression =
+    Var String                   -- Variable
+  | Val Int                      -- Integer literal
+  | Op Expression Bop Expression -- Operation
+  deriving (Show, Eq)
+
+-- Binary (2-input) operators
+data Bop = 
+    Plus     
+  | Minus    
+  | Times    
+  | Divide   
+  | Gt
+  | Ge       
+  | Lt  
+  | Le
+  | Eql
+  deriving (Show, Eq)
+
+data Statement =
+    Assign   String     Expression
+  | Incr     String
+  | If       Expression Statement  Statement
+  | While    Expression Statement       
+  | For      Statement  Expression Statement Statement
+  | Sequence Statement  Statement        
+  | Skip
+  deriving (Show, Eq)
+
+type State = String -> Int
+
+-- Exercise 1 -----------------------------------------
+
+extend :: State -> String -> Int -> State
+extend state varName value = getValue state varName value
+
+getValue :: State -> String -> Int -> String -> Int
+getValue state varName value var
+  | varName == var = value
+getValue state _ _ varName = state varName
+
+empty :: State
+empty = \x -> 0
+
+-- Exercise 2 -----------------------------------------
+
+evalE :: State -> Expression -> Int
+-- init variable
+evalE st (Var varName) = 0
+evalE st (Val value) = value
+evalE st (Op expr1 op expr2) = let expr1Val = evalE st expr1
+                                   expr2Val = evalE st expr2
+                               in
+                                 case op of
+                                   Plus -> expr1Val + expr2Val
+                                   Minus -> expr1Val - expr2Val
+                                   Times -> expr1Val * expr2Val
+                                   Divide -> expr1Val `div` expr2Val
+                                   Gt -> if expr1Val > expr2Val then 1 else 0
+                                   Ge -> if expr1Val >= expr2Val then 1 else 0
+                                   Lt -> if expr1Val < expr2Val then 1 else 0
+                                   Le -> if expr1Val <= expr2Val then 1 else 0
+                                   Eql -> if expr1Val == expr2Val then 1 else 0
+
+-- Exercise 3 -----------------------------------------
+
+data DietStatement = DAssign String Expression
+                   | DIf Expression DietStatement DietStatement
+                   | DWhile Expression DietStatement
+                   | DSequence DietStatement DietStatement
+                   | DSkip
+                     deriving (Show, Eq)
+
+desugar :: Statement -> DietStatement
+desugar = undefined
+
+
+-- Exercise 4 -----------------------------------------
+
+evalSimple :: State -> DietStatement -> State
+evalSimple = undefined
+
+run :: State -> Statement -> State
+run = undefined
+
+-- Programs -------------------------------------------
+
+slist :: [Statement] -> Statement
+slist [] = Skip
+slist l  = foldr1 Sequence l
+
+{- Calculate the factorial of the input
+
+   for (Out := 1; In > 0; In := In - 1) {
+     Out := In * Out
+   }
+-}
+factorial :: Statement
+factorial = For (Assign "Out" (Val 1))
+                (Op (Var "In") Gt (Val 0))
+                (Assign "In" (Op (Var "In") Minus (Val 1)))
+                (Assign "Out" (Op (Var "In") Times (Var "Out")))
+
+
+{- Calculate the floor of the square root of the input
+
+   B := 0;
+   while (A >= B * B) {
+     B++
+   };
+   B := B - 1
+-}
+squareRoot :: Statement
+squareRoot = slist [ Assign "B" (Val 0)
+                   , While (Op (Var "A") Ge (Op (Var "B") Times (Var "B")))
+                       (Incr "B")
+                   , Assign "B" (Op (Var "B") Minus (Val 1))
+                   ]
+
+{- Calculate the nth Fibonacci number
+
+   F0 := 1;
+   F1 := 1;
+   if (In == 0) {
+     Out := F0
+   } else {
+     if (In == 1) {
+       Out := F1
+     } else {
+       for (C := 2; C <= In; C++) {
+         T  := F0 + F1;
+         F0 := F1;
+         F1 := T;
+         Out := T
+       }
+     }
+   }
+-}
+fibonacci :: Statement
+fibonacci = slist [ Assign "F0" (Val 1)
+                  , Assign "F1" (Val 1)
+                  , If (Op (Var "In") Eql (Val 0))
+                       (Assign "Out" (Var "F0"))
+                       (If (Op (Var "In") Eql (Val 1))
+                           (Assign "Out" (Var "F1"))
+                           (For (Assign "C" (Val 2))
+                                (Op (Var "C") Le (Var "In"))
+                                (Incr "C")
+                                (slist
+                                 [ Assign "T" (Op (Var "F0") Plus (Var "F1"))
+                                 , Assign "F0" (Var "F1")
+                                 , Assign "F1" (Var "T")
+                                 , Assign "Out" (Var "T")
+                                 ])
+                           )
+                       )
+                  ]
